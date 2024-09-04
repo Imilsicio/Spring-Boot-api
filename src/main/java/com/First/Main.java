b7f569e5eac8f0e08b432cecbd41096919d80f5e
Imilsicio Monteiro <imilsiciomonteiro@gmail.com>
2024-09-04 13:26:59 +0200

    Fianlizando o metodo put

--- src/main/java/com/First/Main.java
+++ src/main/java/com/First/Main.java
@@ -1,13 +1,16 @@
 package com.First;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -60,4 +63,29 @@ public class Main {
     public void deleteCustomer(@PathVariable("customerId") Integer id) {
         customerRepository.deleteById(id);
     }
+
+    @PutMapping("{customerId}")
+    public ResponseEntity<Customer> updateCustomer(@PathVariable("customerId") Integer id,
+            @RequestBody NewCustomerRequest request) {
+
+        // Verifica se o cliente existe no banco de dados
+        Optional<Customer> optionalCustomer = customerRepository.findById(id);
+
+        if (optionalCustomer.isPresent()) {
+            // Atualiza as informações do cliente
+            Customer customer = optionalCustomer.get();
+            customer.setName(request.name());
+            customer.setEmail(request.email());
+            customer.setAge(request.age());
+
+            // Salva as mudanças no banco de dados
+            customerRepository.save(customer);
+
+            return ResponseEntity.ok(customer);
+        } else {
+            // Retorna uma resposta 404 caso o cliente não seja encontrado
+            return ResponseEntity.notFound().build();
+        }
+    }
+
 }
